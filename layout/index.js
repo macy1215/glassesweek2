a813581e80c077e10a0603c8f2ca9821ab9d4527
Maci Huang <mjmj3794@gmail.com>
2023-09-11 00:09:53 +0800

    JS新增

--- layout/index.js
+++ layout/index.js
@@ -1,8 +1,19 @@
 const headerBox =document.querySelector(".headerBox");
+
+const proFessional = document.querySelector(".professional");
 const itemInside1 =document.querySelector(".focuseBox1");
 const itemInside2 =document.querySelector(".focuseBox2");
 const itemInside3 =document.querySelector(".focuseBox3");
 const itemInside4 =document.querySelector(".focuseBox4");
+
+const classicTitle =document.querySelector(".classicTitle");
+const classicSection =document.querySelector(".classic");
+const classicBox=document.querySelector(".classicBox");
+
+const bannerTitle=document.querySelector(".bannerTitle2")
+const bannerSection=document.querySelector(".banner")
+
+
 const titleSection2 = document.querySelector(".titleSection2");
 
 gsap.registerPlugin(ScrollTrigger);
@@ -21,6 +32,10 @@ gsap.from(titleSection2,{
     opacity:0,
     duration:1,
     x:100,
+    scrollTrigger:{
+        trigger: titleSection2,
+        
+    },
 })
 
 gsap.from(itemInside1,{
@@ -28,18 +43,58 @@ gsap.from(itemInside1,{
     duration:2,
     ease:"power4.out",
     scrollTrigger:{
+        trigger: titleSection2,
+    },
+})
+gsap.from(itemInside2,{
+    x:100,
+    duration:1,
+    ease:"power4.out",
+    scrollTrigger:{
         trigger: itemInside1,
     },
-},"-=0.2")
-timeline.from(itemInside2,{
+})
+gsap.from(itemInside3,{
     x:100,
     duration:1,
-},"-=0.2")
-timeline.from(itemInside3,{
+    ease:"power4.out",
+    scrollTrigger:{
+        trigger: itemInside2,
+    },
+})
+gsap.from(itemInside4,{
     x:100,
     duration:1,
-},"-=0.2")
-timeline.from(itemInside4,{
-    x:100,
+    ease:"power4.out",
+    scrollTrigger:{
+        trigger: itemInside3,
+    },
+})
+
+gsap.from(classicTitle,{
+    opacity:0,
     duration:1,
-},"-=0.2")
+    x:-100,
+    scrollTrigger:{
+        trigger: classicTitle,
+    },
+})
+
+gsap.from(classicBox,{
+    opacity:0,
+    duration:1,
+    y:300,
+    scrollTrigger:{
+        trigger: classicSection,
+    },
+})
+
+gsap.from(bannerTitle,{
+    opacity:0,
+    duration:1,
+    x:100,
+    scrollTrigger:{
+        trigger: bannerSection,
+    },
+    
+})
